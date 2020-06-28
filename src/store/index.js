58c30526b31976284939ea5ce20273d22689d808
Ginakira <ginakira@foxmail.com>
2020-06-28 17:27:24 +0800

    feat: vuex认证状态

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ export default new Vuex.Store({
     username: "",
     is_staff: false,
     is_superuser: false,
+    is_approved: false,
     // Toast
     toastVisible: false,
     toastText: "",
@@ -20,21 +21,25 @@ export default new Vuex.Store({
       state.username = user.username;
       state.is_staff = user.is_staff;
       state.is_superuser = user.is_superuser;
+      state.is_approved = user.is_approved;
       localStorage.setItem("username", state.username);
       localStorage.setItem("is_staff", state.username);
       localStorage.setItem("is_superuser", state.is_superuser);
+      localStorage.setItem("is_approved", state.is_approved);
     },
     loadInfo(state) {
       state.logged = true;
       state.username = localStorage.getItem("username");
       state.is_staff = localStorage.getItem("is_staff") === "true";
       state.is_superuser = localStorage.getItem("is_superuser") === "true";
+      state.is_approved = localStorage.getItem("is_approved") === "true";
     },
     logout(state) {
       state.logged = false;
       state.username = "";
       state.is_staff = false;
       state.is_superuser = false;
+      state.is_approved = false;
       localStorage.clear();
     },
     makeToast(state, payload) {
